320baa0106daaee371694c2333dabd5560d1ff3d
cai <caicai105230@gmail.com>
2021-06-22 16:18:58 +0800

    'update'

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -15,11 +15,10 @@ class Index
         $queue = 'send_mail';
         // 数据，可以直接传数组，无需序列化
         $data = ['to' => 'tom@gmail.com', 'content' => 'hello'];
-        // 投递消息
-        Client::send($queue, $data);
-        // 投递延迟消息，消息会在60秒后处理
-        Client::send($queue, $data, 0);
-
+        for ($i = 0; $i < 1000; $i++) {
+            // 投递延迟消息，消息会在60秒后处理
+            Client::send($queue, $data, 10);
+        }
         return json(['code' => 0, 'msg' => 'ok', 'data' => $user]);
     }
 
